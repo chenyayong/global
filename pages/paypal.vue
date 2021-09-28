cf62c0e478955b726eb1c63910284f6f179411e4
chenyayong <451998731@qq.com>
2021-09-28 18:33:23 +0800

    基础版本

---
+++ pages/paypal.vue
@@ -0,0 +1,23 @@
+<template>
+	<view>
+		<web-view :src="url"></web-view>
+	</view> 
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				url:''
+			};
+		},
+		onLoad(opt) {
+			// console.log(this.$image + '/mobile/index/checkout');
+			this.url = this.$image + '/mobile/index/checkout?id=' + opt.id
+		}
+	}
+</script>
+
+<style lang="scss">
+	
+</style>
